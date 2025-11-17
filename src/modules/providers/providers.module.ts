e57d891351b22c5d13ecff787d5801abcc292c1a
Robson Benevenuto D'Avila Reis <talktorobson@gmail.com>
2025-11-17 22:43:18 +0100

    feat: add provider ranking funnel transparency

--- src/modules/providers/providers.module.ts
+++ src/modules/providers/providers.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@/common/prisma/prisma.module';
 import { ProvidersService } from './providers.service';
+import { ProviderRankingService } from './provider-ranking.service';
 import { ProvidersController } from './providers.controller';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ProvidersController],
-  providers: [ProvidersService],
-  exports: [ProvidersService],
+  providers: [ProvidersService, ProviderRankingService],
+  exports: [ProvidersService, ProviderRankingService],
 })
 export class ProvidersModule {}
